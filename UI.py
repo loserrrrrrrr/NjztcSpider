b9689dc30cb35b6a1e11f2221c4a01db2f442b9d
loserrrrrrrr <46338921+loserrrrrrrr@users.noreply.github.com>
2020-07-08 06:49:18 +0800

    新增图像界面响应

--- UI.py
+++ UI.py
@@ -1,25 +1,32 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-
+import numpy as np
 import duduSpider
 import njzySpider
 import time
 from ui.SpiderUI import Ui_NjztcSpider
-
+from pyGraph import Ui_Graph
 
 
 class mywindow(QWidget, Ui_NjztcSpider):
-    def  __init__ (self):
+    def __init__(self):
         super(mywindow, self).__init__()
         self.setupUi(self)
 
-        self.model = QStandardItemModel(0,8)
-        self.model.setHorizontalHeaderLabels(['作业内容','作业面积','价格/亩','起始时间','结束时间','发布人','联系方式','地址'])  # 设置每列标题
+        self.model = QStandardItemModel(0, 8)
+        self.model.setHorizontalHeaderLabels(
+            ['作业内容', '作业面积（亩）', '价格（元/亩）', '起始时间', '结束时间', '发布人', '联系方式', '地址'])  # 设置每列标题
 
         self.SupplyInform.setModel(self.model)
 
-        self.spiderButton.clicked.connect(lambda:self.spiderOn())
+        # 新建图表子窗口，并传输数据
+        self.dia = Ui_Graph(supply_list)
+        #self.dia.setupUi()
+
+        self.spiderButton.clicked.connect(lambda: self.spiderOn())
+        self.graphButton.clicked.connect(lambda:self.dia.show())
+
 
     def spiderOn(self):
         self.spiderButton.setDisabled(True)
@@ -28,14 +35,30 @@ class mywindow(QWidget, Ui_NjztcSpider):
         for li in supply_list:
             QApplication.processEvents(QEventLoop.ExcludeUserInputEvents)
             if li.if_valid():
-                self.model.appendRow([QStandardItem(str(li.type)), QStandardItem(str(li.area)), QStandardItem(str(li.price)),
-                                      QStandardItem(str(li.date_from)), QStandardItem(str(li.date_to)), QStandardItem(str(li.name)),
-                                      QStandardItem(str(li.tele_num)), QStandardItem(str(li.place))])
+                self.model.appendRow(
+                    [QStandardItem(str(li.type)), QStandardItem(str(li.area)), QStandardItem(str(li.price)),
+                     QStandardItem(str(li.date_from)), QStandardItem(str(li.date_to)), QStandardItem(str(li.name)),
+                     QStandardItem(str(li.tele_num)), QStandardItem(str(li.place))])
             else:
                 continue
 
+def abnormalValue(data=[]):
+    area = [data[i].area for i in range(len(data))]
+    price = [data[i].price for i in range(len(data))]
 
-if __name__=="__main__":
+    area_u = np.mean(area)
+    price_u = np.mean(price)
+
+    area_std = np.std(area)
+    price_std = np.std(price)
+
+    for e in data:
+        if e.area<1e-4 or e.price<1e-4 or e.price>1e5 or np.abs(e.area - area_u) > 3*area_std or np.abs(e.price - price_u) > 3*price_std:
+            data.remove(e)
+
+    return data
+
+if __name__ == "__main__":
     import sys
 
     njzy_url = 'http://njzy.njztc.com/find_taskWorkList.jspx'
@@ -48,8 +71,9 @@ if __name__=="__main__":
     # self.SpiderInform.setText("正在爬取" + dudu_url + "，请稍后")
     d_list = duduSpider.web_spider(dudu_url)
     print(dudu_url + "爬取完毕")
-    supply_list = n_list + d_list
-    app=QApplication(sys.argv)
+    supply_list = abnormalValue(n_list + d_list)
+
+    app = QApplication(sys.argv)
     ui = mywindow()
     ui.show()
     sys.exit(app.exec_())
